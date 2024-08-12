73295d58d938a001535116eea651434b18de1adf
Fred George <fredgeorge@acm.org>
2024-08-12 11:02:01 +0200

    Fred: Rectangle implementation

--- exercises/src/main/java/com/nrkei/training/oo/rectangle/Rectangle.java
+++ exercises/src/main/java/com/nrkei/training/oo/rectangle/Rectangle.java
@@ -1,12 +1,28 @@
 /*
- * Copyright (c) 2024 by Fred George
+ * Copyright (c) 2022 by Fred George
  * May be used freely except for training; license required for training.
  * @author Fred George  fredgeorge@acm.org
  */
 
 package com.nrkei.training.oo.rectangle;
 
-// JOB_DESCRIPTION_GOES_HERE
+// Understands a four-sided polygon with sides at right angles
 public class Rectangle {
-    
+
+    private final double length;
+    private final double width;
+
+    public Rectangle(double length, double width) {
+        if (length <= 0 || width <= 0) throw new IllegalArgumentException("Dimensions must be bigger than zero");
+        this.length = length;
+        this.width = width;
+    }
+
+    public double area() {
+        return length * width;
+    }
+
+    public double perimeter() {
+        return 2 * (length + width);
+    }
 }
--- tests/src/test/java/com/nrkei/training/oo/unit/RectangleTest.java
+++ tests/src/test/java/com/nrkei/training/oo/unit/RectangleTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 by Fred George
+ * Copyright (c) 2022 by Fred George
  * May be used freely except for training; license required for training.
  * @author Fred George  fredgeorge@acm.org
  */
@@ -16,5 +16,14 @@ class RectangleTest {
 
     @Test void area() {
         assertEquals(24.0, new Rectangle(4.0, 6.0).area());
+    }
+
+    @Test void perimeter() {
+        assertEquals(20.0, new Rectangle(4.0, 6.0).perimeter());
+    }
+
+    @Test void parameterRanges() {
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(0, 6));
+        assertThrows(IllegalArgumentException.class, () -> new Rectangle(4, 0));
     }
 }

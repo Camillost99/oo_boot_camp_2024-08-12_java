f7c7c1c6ca680d726afa5f82fd465b3f0361c178
Fred George <fredgeorge@acm.org>
2024-08-13 08:49:08 +0200

    Fred: Distance and Volume are not equal and cannot participate in arithmetic with each other

--- exercises/src/main/java/com/nrkei/training/oo/quantities/Unit.java
+++ exercises/src/main/java/com/nrkei/training/oo/quantities/Unit.java
@@ -28,13 +28,16 @@ public class Unit {
     public static final Unit MILE = new Unit(8, FURLONG);
     public static final Unit LEAGUE = new Unit(3, MILE);
 
+    private final Unit baseUnit;
     private final double baseUnitRatio;
 
     private Unit() {
+        baseUnit = this;
         baseUnitRatio = 1.0;
     }
 
     private Unit(double relativeRatio, Unit relativeUnit) {
+        baseUnit = relativeUnit.baseUnit;
         baseUnitRatio = relativeRatio * relativeUnit.baseUnitRatio;
     }
 
@@ -47,10 +50,15 @@ public class Unit {
     }
 
     double convertedAmount(double otherAmount, Unit other) {
+        assert this.isCompatible(other);
         return otherAmount * other.baseUnitRatio / this.baseUnitRatio;
     }
 
     int hashCode(double amount) {
         return Objects.hashCode(amount * baseUnitRatio);
+    }
+
+    boolean isCompatible(Unit other) {
+        return this.baseUnit == other.baseUnit;
     }
 }

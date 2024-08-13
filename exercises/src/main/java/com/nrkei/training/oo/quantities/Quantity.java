98fc4020ac02adfdbd2c53babf4d61db2520b128
Fred George <fredgeorge@acm.org>
2024-08-13 08:31:54 +0200

    Fred: Missing Quantity class

---
+++ exercises/src/main/java/com/nrkei/training/oo/quantities/Quantity.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2024 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+
+package com.nrkei.training.oo.quantities;
+
+// Understands a specific measurement
+public class Quantity {
+    private final double amount;
+    private final Unit unit;
+
+    Quantity(double amount, Unit unit) {
+        this.amount = amount;
+        this.unit = unit;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this == obj || obj instanceof Quantity && this.equals((Quantity)obj);
+    }
+
+    private boolean equals(Quantity other) {
+        return this.amount == convertedAmount(other);
+    }
+
+    private double convertedAmount(Quantity other) {
+        return this.unit.convertedAmount(other.amount, other.unit);
+    }
+
+    @Override
+    public int hashCode() {
+        return unit.hashCode(amount);
+    }
+
+    public Quantity plus(Quantity other) {
+        return new Quantity(this.amount + convertedAmount(other), this.unit);
+    }
+
+    public Quantity negate() {
+        return new Quantity(-amount, unit);
+    }
+
+    public Quantity minus(Quantity other) {
+        return this.plus(other.negate());
+    }
+}

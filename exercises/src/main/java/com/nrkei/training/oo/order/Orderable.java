ebda958dac19e925e598d31092496de532dab665
Fred George <fredgeorge@acm.org>
2024-08-13 13:26:17 +0200

    Fred: Find the Rectangle with the largest area using streams

--- exercises/src/main/java/com/nrkei/training/oo/order/Orderable.java
+++ exercises/src/main/java/com/nrkei/training/oo/order/Orderable.java
@@ -6,13 +6,15 @@
 
 package com.nrkei.training.oo.order;
 
+import java.util.Arrays;
+
 // Understands specific sequences of elements
 public interface Orderable<T> {
     boolean isBetterThan(T other);
 
     static <S extends Orderable<S>> S best(S first, S... elements) {
-        S champion = first;
-        for (S challenger: elements) if (challenger.isBetterThan(champion)) champion = challenger;
-        return champion;
+        return Arrays.stream(elements).reduce(
+                first,
+                (champion, challenger) -> challenger.isBetterThan(champion) ? challenger : champion);
     }
 }

a200e82f7a7753b586cbcf9e51ff30fccc55fb4b
Fred George <fredgeorge@acm.org>
2024-08-14 08:40:42 +0200

    Fred: Graph with hopCount(); not necessarily minimum; duplicate recursion exists

--- exercises/src/main/java/com/nrkei/training/oo/graph/Node.java
+++ exercises/src/main/java/com/nrkei/training/oo/graph/Node.java
@@ -11,7 +11,9 @@ import java.util.List;
 
 // Understands its neighbors
 public class Node {
+    private static final int UNREACHABLE = -1;
     private final List<Node> neighbors = new ArrayList<>();
+
     public Node to(Node neighbor) {
         neighbors.add(neighbor);
         return neighbor;
@@ -19,6 +21,23 @@ public class Node {
 
     public boolean canReach(Node destination) {
         return canReach(destination, noVisitedNodes());
+    }
+
+    public int hopCount(Node destination) {
+        int result = hopCount(destination, noVisitedNodes());
+        if (result == UNREACHABLE) throw new IllegalArgumentException("Destination not reachable");
+        return result;
+    }
+
+    private int hopCount(Node destination, List<Node> visitedNodes) {
+        if (this == destination) return 0;
+        if (visitedNodes.contains(this)) return UNREACHABLE;
+        visitedNodes.add(this);
+        for (Node n: neighbors) {
+            int neighborHopCount = n.hopCount(destination, visitedNodes);
+            if (neighborHopCount != UNREACHABLE) return neighborHopCount + 1;
+        }
+        return UNREACHABLE;
     }
 
     private boolean canReach(Node destination, List<Node> visitedNodes) {
